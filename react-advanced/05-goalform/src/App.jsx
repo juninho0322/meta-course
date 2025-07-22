c0ea7540400b54817835784599a346c676313c17
juninho0322 <juninho0322@gmail.com>
2025-07-22 18:14:03 +0100

    forms with objects

---
+++ react-advanced/05-goalform/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import './App.css'
+
+function GoalForm(props){
+  
+  const [formData, setFormData] = useState({
+    goal: '',
+    by: '',
+  });
+
+  function changeHandler(e){
+    setFormData({...formData, [e.target.name]: e.target.value});
+  };
+
+  function submitHandler(e){
+    e.preventDefault();
+    props.onAdd(formData)
+    setFormData({
+      goal: '',
+      by: '',
+    });
+  }
+
+  return (
+    <>
+     <h1>My goals</h1>
+     <form onSubmit={submitHandler}>
+      <input type="text" name='goal' placeholder='Goal' value={formData.goal} onChange={changeHandler}  />
+      <input type="text" name='by' placeholder='By' value={formData.by} onChange={changeHandler}  />
+      <button type='submit'>Add Goal</button>
+     </form>
+    </>
+  )
+}
+
+
+function ListOfGoals(props){
+  return (
+    <ul>
+      {props.allGoals.map((g) => (
+        <li key={g.goal}>
+          <span>My goals is {g.goal}, by {g.by}</span>
+        </li>  
+      )
+      )}
+    </ul>
+  );
+}
+
+function App() {
+
+  const [allGoals, updateAllGoals] = useState([]);
+
+  function addGoal(goal) {updateAllGoals([...allGoals, goal]);}
+  
+
+  return (
+    <>
+      <GoalForm onAdd={addGoal} />
+      <ListOfGoals allGoals={allGoals} />
+    </>
+  )
+}
+
+export default App
